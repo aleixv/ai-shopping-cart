3fd6a0c28c924af7dc0612ee48eac02f898327a2
aleixv <aleixverges@gmail.com>
2025-05-09 11:07:35 +0200

    Re add integration test

--- src/test/java/com/example/ai_shopping_cart/AddItemToCartIntegrationTest.java
+++ src/test/java/com/example/ai_shopping_cart/AddItemToCartIntegrationTest.java
@@ -10,11 +10,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.util.List;
 import java.util.UUID;
@@ -23,7 +18,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@Testcontainers
 public class AddItemToCartIntegrationTest {
 
     @Autowired
@@ -31,16 +25,6 @@ public class AddItemToCartIntegrationTest {
 
     @Autowired
     private CartRepository cartRepository;
-
-    @Container
-    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16.3-alpine");
-
-    @DynamicPropertySource
-    static void configureProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", postgres::getJdbcUrl);
-        registry.add("spring.datasource.username", postgres::getUsername);
-        registry.add("spring.datasource.password", postgres::getPassword);
-    }
 
     @Test
     public void testAddItemToCart() {
