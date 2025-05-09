f83720a37ccd1498fb0da2b9c47d51a1bbae61e5
aleixv <aleixverges@gmail.com>
2025-05-09 10:03:00 +0200

    Removed autogenerated value from cart item

--- src/main/java/com/example/ai_shopping_cart/domain/CartItem.java
+++ src/main/java/com/example/ai_shopping_cart/domain/CartItem.java
@@ -11,7 +11,6 @@ import java.util.UUID;
 @Entity
 public class CartItem {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private UUID productId;
     private String name;
