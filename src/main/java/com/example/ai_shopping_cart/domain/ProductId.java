929e1a61dffc5afbb908b0a7a6605b28901c8bcb
aleixv <aleixverges@gmail.com>
2025-05-09 10:02:16 +0200

    Annotated ProductId

--- src/main/java/com/example/ai_shopping_cart/domain/ProductId.java
+++ src/main/java/com/example/ai_shopping_cart/domain/ProductId.java
@@ -1,8 +1,18 @@
 package com.example.ai_shopping_cart.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 import java.util.UUID;
 
+@Entity
 public class ProductId {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
 
     public ProductId(UUID id) {
