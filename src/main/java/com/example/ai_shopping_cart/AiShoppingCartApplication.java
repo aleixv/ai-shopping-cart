72581191e45670cf292cffcef9a17b8a9ad4cc19
aleixv <aleixverges@gmail.com>
2025-05-09 09:27:07 +0200

    Converted project to Gradle and added necessary dependencies

--- src/main/java/com/example/ai_shopping_cart/AiShoppingCartApplication.java
+++ src/main/java/com/example/ai_shopping_cart/AiShoppingCartApplication.java
@@ -2,8 +2,10 @@ package com.example.ai_shopping_cart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.example.ai_shopping_cart.infrastructure")
 public class AiShoppingCartApplication {
 
     public static void main(String[] args) {
