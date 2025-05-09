38384c42fcffd2276309d42232d98246c8dfaf8d
aleixv <aleixverges@gmail.com>
2025-05-09 11:26:26 +0200

    Remove 2 SpringbootTest annotations

--- src/test/java/com/example/ai_shopping_cart/AddItemToCartIntegrationTest.java
+++ src/test/java/com/example/ai_shopping_cart/AddItemToCartIntegrationTest.java
@@ -8,7 +8,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
-@SpringBootTest
 public class AddItemToCartIntegrationTest {
 
     @Container
