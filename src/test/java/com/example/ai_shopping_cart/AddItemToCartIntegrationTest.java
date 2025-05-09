7a84af3e4b53a5d0bcf12922d1200c5a56aea09a
aleixv <aleixverges@gmail.com>
2025-05-09 11:25:54 +0200

    Remove test imports

--- src/test/java/com/example/ai_shopping_cart/AddItemToCartIntegrationTest.java
+++ src/test/java/com/example/ai_shopping_cart/AddItemToCartIntegrationTest.java
@@ -6,8 +6,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
 
 @SpringBootTest
 public class AddItemToCartIntegrationTest {
